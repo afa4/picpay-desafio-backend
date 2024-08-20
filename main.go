179c816405a2d61f1fcbb8b99a38402a181b95de
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-08-20 11:54:15 -0300

    feat: transaction (transfer) handler

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 type Transaction struct {
@@ -14,8 +15,10 @@ type Transaction struct {
 }
 
 func main() {
+	transactionChannel := make(chan Transaction)
+	go transactionRoutine(&transactionChannel)
 	http.HandleFunc("/", getRootHandler())
-	http.HandleFunc("/transfer", postTransactionHandler())
+	http.HandleFunc("/transfer", postTransactionHandler(&transactionChannel))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -29,7 +32,7 @@ func getRootHandler() http.HandlerFunc {
 	}
 }
 
-func postTransactionHandler() http.HandlerFunc {
+func postTransactionHandler(transactionChannel *chan Transaction) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -42,8 +45,19 @@ func postTransactionHandler() http.HandlerFunc {
 			http.Error(w, "Error reading request body", http.StatusInternalServerError)
 			return
 		}
-
+		*transactionChannel <- transaction
+		w.WriteHeader(http.StatusAccepted)
 		w.Write([]byte("Hello World!" + " " + fmt.Sprintf("%d", transaction.Payer) + " " + fmt.Sprintf("%d", transaction.Payee) + " " + fmt.Sprintf("%f", transaction.Amount)))
+	}
+}
+
+func transactionRoutine(transactionChannel *chan Transaction) {
+	fmt.Println("Transaction routine started")
+	for {
+		transaction := <-*transactionChannel
+		time.Sleep(1 * time.Second)
+		fmt.Println("Transaction received")
+		fmt.Println(transaction)
 	}
 }
 
